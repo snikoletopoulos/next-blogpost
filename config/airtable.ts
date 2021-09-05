f8c1d19f8745389f8dd1a007ecc6594cc46863ce
Stavros <snikoletopoulos@hotmail.com>
2021-09-05 18:42:19 +0300

    Formating

--- config/airtable.ts
+++ config/airtable.ts
@@ -1,20 +1,18 @@
 import Airtable, { Error } from "airtable";
+
 const base = new Airtable({ apiKey: process.env.API_KEY }).base(
 	process.env.BASE_ID
 );
 
 export const savePost = ({ name, email, blogurl, feedurl, notes }) => {
-	return base("Blogs").create([
-		{
+	return base("Blogs").create(
+		[{ fields: { name, email, blogurl, feedurl, notes } }],
 			fields: { name, email, blogurl, feedurl, notes },
-		},
-		,
-		err => {
 			if (err) {
 				console.log(err);
 				return false;
 			}
 			return true;
-		},
-	]);
+		}
+	);
 }
