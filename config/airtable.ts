94ddfc67156db945941bed52277d1776438fe8d4
Stavros <snikoletopoulos@hotmail.com>
2021-09-05 18:42:46 +0300

    Type err variable

--- config/airtable.ts
+++ config/airtable.ts
@@ -7,7 +7,7 @@ const base = new Airtable({ apiKey: process.env.API_KEY }).base(
 export const savePost = ({ name, email, blogurl, feedurl, notes }) => {
 	return base("Blogs").create(
 		[{ fields: { name, email, blogurl, feedurl, notes } }],
-			fields: { name, email, blogurl, feedurl, notes },
+		(err: Error) => {
 			if (err) {
 				console.log(err);
 				return false;
