854a11410d68c647f9424a2ac48dc6454c65fca8
Stavros <snikoletopoulos@hotmail.com>
2021-09-05 18:40:54 +0300

    Convert to arrow function

--- config/airtable.ts
+++ config/airtable.ts
@@ -3,7 +3,7 @@ const base = new Airtable({ apiKey: process.env.API_KEY }).base(
 	process.env.BASE_ID
 );
 
-export default function savePost({ name, email, blogurl, feedurl, notes }) {
+export const savePost = ({ name, email, blogurl, feedurl, notes }) => {
 	return base("Blogs").create([
 		{
 			fields: { name, email, blogurl, feedurl, notes },
