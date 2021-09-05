9048b1458bba5ea3040c02609cf44b37b5d83f97
Stavros <snikoletopoulos@hotmail.com>
2021-09-05 17:05:19 +0300

    Change name function savePost

--- config/airtable.ts
+++ config/airtable.ts
@@ -3,18 +3,12 @@ const base = new Airtable({ apiKey: process.env.API_KEY }).base(
 	process.env.API_BASE
 );
 
-export default function airtableSavePost({
-	name,
-	email,
-	blogurl,
-	feedurl,
-	notes,
-}) {
-	base("Blogs").create(
+export default function savePost({ name, email, blogurl, feedurl, notes }) {
+	return base("Blogs").create([
 		{
 			fields: { name, email, blogurl, feedurl, notes },
 		},
-		],
+		,
 		err => {
 			if (err) {
 				console.log(err);
