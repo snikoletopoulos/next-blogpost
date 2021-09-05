2f24bdac73af8e4d151765b77e58a8a971516eee
Stavros <snikoletopoulos@hotmail.com>
2021-09-05 15:16:26 +0300

    Initial config for Airtable

---
+++ config/airtable.ts
@@ -0,0 +1,23 @@
+const Airtable = require("airtable");
+const base = new Airtable({ apiKey: process.env.API_KEY }).base(
+	process.env.API_BASE
+);
+
+export default function pushPostToAirtable({
+	name,
+	email,
+	blogurl,
+	feedurl,
+	notes,
+}) {
+	base("Blogs").create([
+		{
+			fields: { name, email, blogurl, feedurl, notes },
+		},
+	],
+  err => {
+    console.log(err);
+    return false;
+  }
+  );
+}
