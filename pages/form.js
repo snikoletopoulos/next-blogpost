1dcccb92086660c7911b133926e8055fc0e5d8d4
Stavros <snikoletopoulos@hotmail.com>
2021-09-05 11:51:07 +0300

    Add all files

---
+++ pages/form.js
@@ -0,0 +1,155 @@
+import React, { useState } from "react";
+import Head from "next/head";
+import Link from "next/link";
+import Router from "next/router";
+
+export default function Form() {
+	const [name, setName] = useState("");
+	const [email, setEmail] = useState("");
+	const [blogurl, setBlogurl] = useState("");
+	const [feedurl, setFeedurl] = useState("");
+	const [notes, setNotes] = useState("");
+	const [response, setResponse] = useState("initialState");
+
+	const handleSubmit = async event => {
+		event.preventDefault();
+
+		try {
+			const response = await fetch("/api/blog", {
+				method: "POST",
+				headers: { "Content-Type": "application/json" },
+				body: JSON.stringify({ name, email, blogurl, feedurl, notes }),
+			});
+
+			const json = await response.json();
+			if (json.success) {
+				alert("Thank you for submitting your blog!");
+				Router.push("/");
+			} else {
+				setResponse(json.success);
+			}
+		} catch (err) {
+			setResponse("An error occured while submitting the form");
+		}
+	};
+
+	return (
+		<>
+			<Head>
+				<title>Add new blog</title>
+				<link rel="icon" href="/favicon.ico" />
+				<link
+					rel="stylesheet"
+					href="https://cdn.jsdelivr.net/npm/@tailwindcss/ui@latest/dist/tailwind-ui.min.css"
+				/>
+			</Head>
+			<header className="bg-white shadow">
+				<div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+					<div className="flex justify-between">
+						<h1 className="text-3xl font-bold leading-tight text-gray-900">
+							Add new blog
+						</h1>
+						<p>
+							<Link href="/">
+								<p className="underline cursor-pointer mt-2">
+									<a>Back</a>
+								</p>
+							</Link>
+						</p>
+					</div>
+				</div>
+			</header>
+
+			<main>
+				<p className="text-center pb-5">{response}</p>
+
+				<div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+					<div>
+						<div className="max-w-3xl mx-auto sm:px-6 lg:px-8">
+							<form
+								className="mt-5 md:mt-0 md:col-span-2"
+								action=""
+								method="POST"
+								onSubmit={handleSubmit}>
+								<div className="shadow sm:rounded-md sm:overflow-hidden">
+									<div className="px-4 py-5 bg-white sm:p-6">
+										<label className="block text-sm font-medium leading-5 text-gray-700">
+											Blog name / owner name
+										</label>
+										<input
+											required
+											className="mb-5 mt-1 form-input block w-full py-2 px-3 border border-gray-300 rounded shadow-sm focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5"
+											value={name}
+											onChange={event => setName(event.target.value)}
+										/>
+										<label className="block text-sm font-medium leading-5 text-gray-700">
+											Email address
+										</label>
+										<input
+											required
+											type="email"
+											className="mb-5 mt-1 form-input block w-full py-2 px-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5"
+											value={email}
+											onChange={event => setEmail(event.target.value)}
+										/>
+										<label className="block text-sm font-medium leading-5 text-gray-700">
+											Blog URL
+										</label>
+										<input
+											type="url"
+											required
+											className="mb-5 mt-1 form-input block w-full py-2 px-3 border border-gray-300 rounded shadow-sm focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5"
+											placeholder="https://www.example.com"
+											value={blogurl}
+											onChange={event => setBlogurl(event.target.value)}
+										/>
+										<label className="block text-sm font-medium leading-5 text-gray-700">
+											RSS Feed URL
+										</label>
+										<input
+											type="url"
+											required
+											className="mb-5 mt-1 form-input block w-full py-2 px-3 border border-gray-300 rounded shadow-sm focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5"
+											placeholder="https://www.example.com/feed"
+											value={feedurl}
+											onChange={event => setFeedurl(event.target.value)}
+										/>
+
+										<label
+											htmlFor="about"
+											className="block text-sm leading-5 font-medium text-gray-700">
+											Notes
+										</label>
+										<div className="rounded-md shadow-sm">
+											<textarea
+												rows="3"
+												className="form-textarea mt-1 block w-full transition duration-150 ease-in-out sm:text-sm sm:leading-5"
+												placeholder="Anything you want to tell us!"
+												value={notes}
+												onChange={event =>
+													setNotes(event.target.value)
+												}></textarea>
+										</div>
+										<p className="mt-2 text-sm text-gray-500">
+											Your submission will be approved before appearing on the
+											site
+										</p>
+									</div>
+									<div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
+										<span className="inline-flex rounded-md shadow-sm">
+											<button
+												type="submit"
+												className="inline-flex justify-center py-2 px-4 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-blue-600 hover:bg-blue-500 focus:outline-none focus:border-blue-700 focus:shadow-outline-blue active:bg-blue-700 transition duration-150 ease-in-out">
+												Save
+											</button>
+										</span>
+									</div>
+								</div>
+							</form>
+						</div>
+					</div>
+				</div>
+			</main>
+		</>
+	);
+}
---
+++ pages/index.js
@@ -0,0 +1,113 @@
+import React from "react";
+import Head from "next/head";
+import Parser from "rss-parser";
+import Link from 'next/link';
+
+const Home = props => {
+	return (
+		<>
+			<Head>
+				<title>Latest posts</title>
+				<link rel="icon" href="/favicon.ico" />
+				<link
+					rel="stylesheet"
+					href="https://cdn.jsdelivr.net/npm/@tailwindcss/ui@latest/dist/tailwind-ui.min.css"
+				/>
+			</Head>
+
+			<header className="bg-white shadow">
+				<div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+					<div className="flex justify-between">
+						<h1 className="text-3xl font-bold leading-tight text-gray-900">
+							Latest posts
+						</h1>
+						<p>
+							<Link href="/form">
+								<p className="underline cursor-pointer mt-2">
+									<a>Add a new blog</a>
+								</p>
+							</Link>
+						</p>
+					</div>
+				</div>
+			</header>
+			<main>
+				<div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+					<div className="px-4 py-4 sm:px-0">
+						<div className="border-4 rounded-lg">
+							<div className="flex flex-col">
+								<div className="-my-2 py-2 overflow-x-auto sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8">
+									<div className="align-middle inline-block min-w-full shadow overflow-hidden sm:rounded-lg">
+										<table className="min-w-full">
+											<thead>
+												<tr>
+													<th className="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+														Post
+													</th>
+													<th className="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+														Date
+													</th>
+												</tr>
+											</thead>
+											<tbody className="bg-white">
+												{props.posts
+													.sort((a, b) => new Date(b.date) - new Date(a.date))
+													.map((value, index) => {
+														return (
+															<tr key={index}>
+																<td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+																	<div className="flex items-center">
+																		<div className="ml-4">
+																			<div className="text-sm leading-5 font-medium text-gray-900 underline">
+																				<a href={value.link}>{value.title}</a>
+																			</div>
+																			<div className="text-sm leading-5 text-gray-500">
+																				{value.name}
+																			</div>
+																		</div>
+																	</div>
+																</td>
+																<td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+																	<div className="text-sm leading-5 text-gray-900">
+																		{new Date(value.date).toDateString()}
+																	</div>
+																	<div className="text-sm leading-5 text-gray-500"></div>
+																</td>
+															</tr>
+														);
+													})}
+											</tbody>
+										</table>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</main>
+		</>
+	);
+};
+
+export async function getStaticProps(context) {
+	const parcer = new Parser();
+
+	const data = await parcer.parseURL("https://flaviocopes.com/index.xml");
+
+	const posts = data.items.slice(0, 10).map(item => {
+		return {
+			title: item.title,
+			link: item.link,
+			date: item.isoDate,
+			name: "Flavio Copes",
+		};
+	});
+
+	return {
+		props: {
+			posts,
+		},
+	};
+}
+
+export default Home;
