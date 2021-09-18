b8cfd1b405ba384530fb67f3fc2df80c6a2981b0
Stavros <snikoletopoulos@hotmail.com>
2021-09-18 22:02:22 +0300

    Change Airtable config file folder name

--- pages/api/get-blog.tsx
+++ pages/api/get-blog.tsx
@@ -1,5 +1,7 @@
-//@ts-ignore
-import { savePost} from "/config/airtable";
+// @ts-expect-error
+import { savePost } from "@/utils/airtable";
+// @ts-expect-error
+import type { Post } from "@/utils/airtable";
 import type { NextApiResponse, NextApiRequest } from "next";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
@@ -9,21 +11,15 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
 		res.end();
 		return;
 	}
-	const { name, email, blogurl, feedurl, notes } = req.body;
-	const post: Post = { name, email, blogurl, feedurl, notes };
 
-	if (savePost(post)) {
+	const { name, email, blogurl, feedurl, notes } = req.body;
+
+	const post: Post = { name, email, blogurl, feedurl, notes };
+	let saved;
+	if (savePost(post, res)) {
 		res.json({ success: true });
 	} else {
-		res.json({ failed: true });
+		console.log("hi");
+		res.json({ failed: true, test: saved, teseta: "hi" });
 	}
-}
-
-interface Post {
-	name: string;
-	email: string;
-	blogurl: string;
-	feedurl: string;
-	notes: string;
-	approved?: boolean;
 }
--- utils/airtable.ts
+++ utils/airtable.ts
@@ -1,18 +1,20 @@
 import Airtable, { Error } from "airtable";
+import type { NextApiResponse } from "next";
 
 const base = new Airtable({ apiKey: process.env.API_KEY }).base(
 	process.env.BASE_ID
 );
 
-export const savePost = ({ name, email, blogurl, feedurl, notes }) => {
+export const savePost = (post: Post, res: NextApiResponse) => {
 	return base("Blogs").create(
-		[{ fields: { name, email, blogurl, feedurl, notes } }],
+		[{ fields: { ...post, mpla: "asfa" } }],
 		(err: Error) => {
 			if (err) {
-				console.log(err);
-				return false;
+				console.error(err);
+				res.statusCode = 500;
+				res.end();
+				return;
 			}
-			return true;
 		}
 	);
 };
@@ -30,3 +32,12 @@ export const getPosts = async () => {
 			};
 		});
 };
+
+export interface Post {
+	name: string;
+	email: string;
+	blogurl: string;
+	feedurl: string;
+	notes: string;
+	approved?: boolean;
+}
